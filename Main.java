738e9aae4d003ed556be018f3e7fa75b6984dd03
Ivan Ramirez Comas <70542717+IvanR-C@users.noreply.github.com>
2021-11-18 11:20:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,44 @@
 import java.util.*;
 
+
 public class Main {
     public static void radixSort(int[] arr) {
 
-        ArrayList<String> bucket = new ArrayList<>();
         ArrayList<ArrayList<String>> output = new ArrayList<>();
 
         String[] strArray = Arrays.stream(arr).mapToObj(String::valueOf).toArray(String[]::new);
 
-        for (int i = 0; i< 5; i++) {
-            output.add(new ArrayList<String>());
+        for (char i = '0'; i < '5'; i++) {
+            output.add(new ArrayList<>());
         }
 
         for (String s : strArray) {
-            if(s.contains("1") || s.contains("3") || s.contains("5") || s.contains("7") || s.contains("9")){
+            if (s.contains("1") || s.contains("3") || s.contains("5") || s.contains("7") || s.contains("9")) {
                 System.out.println("*** Abort *** the input has at least one key with odd digits");
                 return;
             }
-            else{
-                for (int i = 0; i < 5; i++) {
-                    if (s.charAt(s.length()-1) == ((char)i*2)){
-                        output.get(i).add(s);
+        }
+
+        for (String s : strArray) {
+            int x=Integer.parseInt(s);
+            s = String.format("%03d" , x);
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (s.charAt(s.length() - i) == (char) (j * 2 + '0')) {
+                        output.get(j).add(s);
                     }
                 }
             }
         }
 
-        for (int i = 0; i < output.size();i++)
-            System.out.println(output.get(i));
-
-//        System.out.println(Arrays.toString(output));
+        for (ArrayList<String> l1 : output) {
+            for (String n : l1) {
+                int y=Integer.parseInt(n);
+                n = String.format("%01d" , y);
+                System.out.print(n + " ");
+            }
+            System.out.println(" ");
+        }
     }
 
     public static void main(String[] args) {
